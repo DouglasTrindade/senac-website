4613bc7d0220c70f3eec81d8fbd30be41da56f8e
Douglas Trindade <douglastrindade22@hotmail.com>
2023-03-01 00:39:14 -0300

    feat: navbarmenu responsive and buttons topbar responsive

--- ClientApp/src/components/NavMenu/index.jsx
+++ ClientApp/src/components/NavMenu/index.jsx
@@ -1,41 +1,76 @@
 import styles from './styles.module.css';
-import Container from "react-bootstrap/esm/Container";
+
+
+import { Button}  from 'react-bootstrap'
+import { Collapse } from 'react-bootstrap'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NavMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <nav className={`navbar navbar-expand-lg ${styles.navbar}`}>
         <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <Container className="mb-2 mt-2">
-              <ul className={`navbar-nav d-flex gap-5 me-auto mb-2 mb-lg-0 ${styles.menu}`}>
-                <Link to="/">
-                  <li className="nav-item">Início</li>
-                </Link>
-                <Link to="/">
-                  <li className="nav-item">Quem somos</li>
-                </Link>
-                <Link to="/">
-                  <li className="nav-item">Vagas</li>
-                </Link>
-                <Link to="/">
-                  <li className="nav-item">Orientação Profissional</li>
-                </Link>
-                  <Link to="/"><li className="nav-item">Fale Conosco</li>
-                </Link>
-                <Link to="/">
-                  <li className="nav-item">Portal Senac</li>
-                </Link>
-                <Link to="/">
-                  <li className="nav-item">Central de Dúvidas</li>
-                </Link>
-              </ul>
-            </Container>
+          <div className="container mb-2 mt-2 d-none d-sm-none d-lg-block">
+            <ul className={`navbar-nav d-flex gap-5 me-auto mb-2 mb-lg-0 ${styles.menu}`}>
+              <Link to="/">
+                <li className="nav-item">Início</li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">Quem somos</li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">Vagas</li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">Orientação Profissional</li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">Fale Conosco</li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">Portal Senac</li>
+              </Link>
+              <Link to="/">
+                <li className="nav-item">Central de Dúvidas</li>
+              </Link>
+            </ul>
           </div>
+          <Button className="d-none-block d-lg-none d-xl-none d-xxl-none" onClick={() => setIsOpen(!isOpen)} aria-controls="collapse-menu" aria-expanded={isOpen}>
+            <span class="navbar-toggler-icon"></span>
+          </Button>
+            <Collapse in={isOpen}>
+              <div id="collapse-menu">
+                <div className="container mb-2 mt-2">
+                  <ul className={`navbar-nav d-flex gap-5 me-auto mb-2 mb-lg-0 ${styles.menu}`}>
+                    <Link to="/">
+                      <li className="nav-item">Início</li>
+                    </Link>
+                    <Link to="/">
+                      <li className="nav-item">Quem somos</li>
+                    </Link>
+                    <Link to="/">
+                      <li className="nav-item">Vagas</li>
+                    </Link>
+                    <Link to="/">
+                      <li className="nav-item">Orientação Profissional</li>
+                    </Link>
+                    <Link to="/">
+                      <li className="nav-item">Fale Conosco</li>
+                    </Link>
+                    <Link to="/">
+                      <li className="nav-item">Portal Senac</li>
+                    </Link>
+                    <Link to="/">
+                      <li className="nav-item">Central de Dúvidas</li>
+                    </Link>
+                  </ul>
+                </div>
+              </div>
+            </Collapse>
+        
         </div>
       </nav>
     </>
--- ClientApp/src/components/NavTop/index.jsx
+++ ClientApp/src/components/NavTop/index.jsx
@@ -1,18 +1,18 @@
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import styles from './styles.module.css';
 
 
 const NavTop = () => {
+  
+  
   return (
     <>
-      <Container className={styles.container}>
-        <Row>
-          <Col>
+      <Container className={`${styles.container}`}>
+        <div className="row">
+          <div className="mx-4 mx-md-0 col-md-6">
             <img src="images/logo-home.svg" alt="Logo Empregabilidade" />
-          </Col>
-          <Col className="d-flex justify-content-end gap-3">
+          </div>
+          <div className="justify-content-center mt-4 mt-md-0 justify-content-md-end d-flex gap-3 col-md-6">
             <button type="button" className={`d-flex justify-content-center align-items-center gap-2 btn text-white ${styles.btnNav}`}>
               <img src="images/iconUser.png" width={14} height={14} alt="user circle" />
               Candidato
@@ -21,8 +21,8 @@ const NavTop = () => {
               <img src="images/bagWork.png" alt="bag work" />
               Empregador
             </button>
-          </Col>
-        </Row>
+          </div>
+        </div>
       </Container>
     </>
   )
